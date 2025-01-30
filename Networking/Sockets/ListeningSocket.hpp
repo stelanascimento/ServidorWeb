e4c78e99305033927ddc2db79d1a392b16c256c0
snon <snon@g2c45.cin.ufpe.br>
2025-01-30 18:28:40 -0300

    Adding Listening Class

---
+++ Networking/Sockets/ListeningSocket.hpp
@@ -0,0 +1,26 @@
+#ifndef ListeningSocket_hpp
+#define ListeningSocket_hpp
+
+#include <stdio.h>
+
+#include "BindingSocket.hpp"
+
+namespace HDE
+{
+    class ListeningSocket
+    { 
+    private:
+        int backlog;
+        int listening;
+
+    public:
+        // Constructor
+        ListeningSocket(int domain, int service, int protocol, int port, u_long interface, int bklg);
+        //backlog define  o número máximo de conecções pendentes que podem ficar na fila, antes de serm recusadas.
+        //quando começar a escutar, vai ser preciso 2 argumentos pra função
+        void start_listening();
+       
+    };
+}
+
+#endif /* Listening_hpp*/
